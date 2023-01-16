76f04c9f023fb0e61f6adf4a99040c895077c157
BitNers <wesleyalvessilva@outlook.com.br>
2023-01-15 21:47:44 -0300

    Update Startup
    Adding Cookie settings, DbContext, JWT Tokens and Authentication and Authorization.

--- NotesAPI/Program.cs
+++ NotesAPI/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +10,42 @@ builder.Services.AddControllers().AddJsonOptions(opt => {
 });
 
 string conn = builder.Configuration.GetConnectionString("devDB") ?? "NaN";
-builder.Services.AddDbContext<NotesAPI.Database.DatabaseContext>(opt => { opt.UseSqlServer(conn);  });
 
+builder.Services.AddDbContext<NotesAPI.Database.DatabaseContext>(opt => { opt.UseSqlServer(conn); });
+
+
+builder.Services.AddSession(opt => {
+    var minutes = TimeSpan.FromMinutes(10);
+        opt.IdleTimeout = minutes;
+        opt.Cookie.MaxAge = minutes;
+        opt.Cookie.HttpOnly = true;
+        opt.Cookie.IsEssential = true;
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(NotesAPI.AppConfig.Secret)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
