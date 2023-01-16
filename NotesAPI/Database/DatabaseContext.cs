7af165fe951de2725b5171baefdaf7ba9d6d3617
BitNers <wesleyalvessilva@outlook.com.br>
2023-01-15 21:51:08 -0300

    Update Database Context
    Forcing the User Model to have an Unique Key in Email Attribute.

--- NotesAPI/Database/DatabaseContext.cs
+++ NotesAPI/Database/DatabaseContext.cs
@@ -8,6 +8,10 @@ namespace NotesAPI.Database
         public DbSet<Models.UserModel> Users { get; set; }
         public DbSet<Models.NotesModel> Notes { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            modelBuilder.Entity<Models.UserModel>().HasIndex(b => b.Email).IsUnique();
+        }
+
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }  
     }
 }
