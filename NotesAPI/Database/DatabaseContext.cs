61b40af7c68475ee7a262899bc8bd0e9fbdcea33
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-10 17:47:43 -0300

    Update DatabaseContext
    Since the User table is now on "AspNetUsers", this Context for User Model is deprecated.

--- NotesAPI/Database/DatabaseContext.cs
+++ NotesAPI/Database/DatabaseContext.cs
@@ -5,11 +5,11 @@ namespace NotesAPI.Database
     public class DatabaseContext : DbContext
     {
 
-        public DbSet<Models.UserModel> Users { get; set; }
+        //public DbSet<Models.UserModel> Users { get; set; }
         public DbSet<Models.NotesModel> Notes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) { 
-            modelBuilder.Entity<Models.UserModel>().HasIndex(b => b.Email).IsUnique();
+          //   modelBuilder.Entity<Models.UserModel>().HasIndex(b => b.Email).IsUnique();
         }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }  
