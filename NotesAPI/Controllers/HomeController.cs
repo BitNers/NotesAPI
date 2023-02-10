e26f89025ae8f1f8e8bbca441451cbd9e50b1814
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-10 17:45:34 -0300

    Updating Home Controller
    Adding now a new route just to test the token.

--- NotesAPI/Controllers/HomeController.cs
+++ NotesAPI/Controllers/HomeController.cs
@@ -2,16 +2,28 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using OpenIddict.Validation.AspNetCore;
+using System.Security.Claims;
+
 namespace NotesAPI.Controllers
 {
     [Route("/")]
     [ApiController]
-    [AllowAnonymous]
     public class HomeController : ControllerBase
     {
+        
+
         [HttpGet]
         public OkObjectResult Get() {
             return Ok("Hello");
+        }
+
+        [HttpGet("token")]
+        [Authorize]
+        
+        public IActionResult GetwithToken()
+        {
+            return Ok("Hello with Token: "+HttpContext.Request.Headers.Authorization.ToString() ?? string.Empty);
         }
 
     }
